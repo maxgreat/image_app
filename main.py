84df6c4f6cd0e536c0a6302954a1ad5f257c17bf
Maxime Portaz <maximep@graphcore.ai>
2023-12-27 15:01:20 +0100

    Update main.py
    add floating button

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
+from kivy.uix.image import AsyncImage
 from kivy.core.window import Window
 from kivy.lang import Builder
 import os
@@ -20,7 +24,7 @@ def show_full_image(instance, touch):
 
 class MainWindow(StackLayout):
     def __init__(self, **kwargs):
-        super().__init__(orientation='lr-tb', spacing=10, size_hint_y=None, **kwargs)
+        super().__init__(orientation='lr-tb', spacing=(1,1), padding=1, size_hint_y=None, **kwargs)
         self.images_list = []
         path = os.path.join(os.path.expanduser('~'), "OneDrive\Images")
         for f in os.listdir(path):
@@ -30,24 +34,44 @@ class MainWindow(StackLayout):
                 self.add_image(os.path.join(path,f))
 
     def add_image(self, image_path):
-        image = Image(source=image_path, size_hint_y=None, size_hint_x=None,
-                      width=(Window.width - 30) / 3, allow_stretch=True, fit_mode="contain")
-        image.bind(on_touch_down=show_full_image)
-        self.add_widget(image)
+        def update_ui(dt):
+            image = AsyncImage(source=image_path, size_hint_y=None, size_hint_x=None, allow_stretch=True, fit_mode='scale-down')
+            image.bind(on_touch_down=show_full_image)
+            self.add_widget(image)
+        Clock.schedule_once(update_ui)
 
 class MyApp(App):
     def build(self):
         Window.bind(on_resize=self.on_window_resize)
+
         layout = MainWindow()
         layout.bind(minimum_height=layout.setter('height'))
-        self.root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
-        self.root.add_widget(layout)
+        
+        self.root = FloatLayout()
+        self.scroll_view_layout = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        self.scroll_view_layout.add_widget(layout)
+        self.root.add_widget(self.scroll_view_layout)
+
+
+        floating_button = Button(
+            text="+",
+            size_hint=(None, None),
+            size=("48dp", "48dp"),
+            pos_hint={"right": 0.95, "top": 0.95}
+        )
+        floating_button.bind(on_press=self.on_floating_button_press)
+
+
+        self.root.add_widget(floating_button)
         return self.root
      
-
+    
     def on_window_resize(self, window, width, height):
-        for child in self.root.children:
-            print(child.pos)
+        self.scroll_view_layout.size = (Window.width, Window.height)
+    
+    def on_floating_button_press():
+        pass
+   
 
 if __name__ == "__main__":
     MyApp().run()
