f37066f414384bbdb1a61a6e75ae6d98fa24faa0
Maxime Portaz <maximep@graphcore.ai>
2024-01-08 17:33:16 +0100

    move code to main.kv

--- main.py
+++ main.py
@@ -1,86 +1,18 @@
 import os
-#os.environ['KIVY_IMAGE'] = 'PIL'
-from kivy.factory import Factory as F
+import yaml
+
 from kivy.lang import Builder
 from kivy.utils import platform
 from kivy.clock import Clock
-from kivy.properties import StringProperty, ObjectProperty
-from kivy.animation import Animation
 from kivy.uix.behaviors import ButtonBehavior
 from kivymd.uix.behaviors import RectangularRippleBehavior
-from kivymd.uix.fitimage import FitImage
-from kivy.uix.image import AsyncImage, Image
+from kivy.uix.image import AsyncImage
 from kivy.core.window import Window
+from kivy.storage.jsonstore import JsonStore
 
 
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen
 from kivymd.uix.hero import MDHeroFrom
-from kivymd.uix.screen import MDScreen
-
-import yaml
-
-KV = '''
-MDScreenManager:
-    id: screenManager
-    MDScreen:
-        name: "Main Screen"
-        ScrollView:
-            MDGridLayout:
-                id: grid
-                cols: 3
-                spacing: "4dp"
-                padding: "4dp"
-                adaptive_height: True
-        MDFloatingActionButton:
-            icon: "image-plus"
-            md_bg_color: app.theme_cls.primary_color
-            pos_hint: {"center_x": .9, "center_y": .1}
-            on_release: app.add_repo()
-        MDSlider:
-            id: slider
-            pos_hint: {"center_x": .5, "center_y": .1}
-            size_hint: .5, .1
-            min: 1
-            max: 10
-            value: 3
-            step: 1
-            on_value: app.slider_down(*args)
-
-    MDScreen:
-        name: "Photo Screen"
-        heroes_to: [hero_to]
-        MDHeroTo:
-            id: hero_to
-            size_hint: 1, 1
-            pos_hint: {"center_x": 0.5, "center_y":0.5}
-        MDRaisedButton:
-            text: "Super Resolution"
-            pos_hint: {"center_x": .2}
-            y: "36dp"
-            on_release: app.next_image()
-        MDRaisedButton:
-            text: "Back"
-            pos_hint: {"center_x": .5}
-            y: "36dp"
-            on_release:
-                root.current_heroes = [hero_to.tag]
-                root.current = "Main Screen"
-
-<ImageTile>:
-    size_hint_y: None
-    size_hint_x: 1
-    height: "200dp"
-    radius: 24
-    ClickableImage:
-        id: tile
-        size_hint: None, None
-        size: root.size
-        on_release: root.on_release()
-        mipmap: True
-        opacity : 1
-'''
-
 
 
 VALID_IMG_EXT = [".jpg",".png",".tga"]
@@ -109,7 +41,7 @@ class ImageApp(MDApp):
         Window.bind(on_request_close=self.on_request_close)
         self.images_list = {}
         self.models = yaml.safe_load("model_config.yml")
-        return Builder.load_string(KV)
+        return Builder.load_file('main.kv')
     
     def on_start(self):
         if platform == 'android':
@@ -120,9 +52,7 @@ class ImageApp(MDApp):
             path = '/storage/emulated/0/Pictures/'
         elif platform == 'win':
             self.settings_path = self.user_data_dir
-            path = os.path.join(os.path.expanduser('~'), "OneDrive/Images")
-            path = "D://recup/done/perfect/"
-            path = "/"
+            path = os.path.join(os.path.expanduser('~'))
         else:
             path = os.path.join(os.path.expanduser('~'), "/Pictures")
         self.add_images(path)
@@ -154,7 +84,7 @@ class ImageApp(MDApp):
             self.add_images(path[0])
 
     def on_request_close(self, *args):
-        self.textpopup(title='Exit', text='Are you sure?')
+        self.stop()
         return True
 
 
